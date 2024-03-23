db8aff94e0bbedaa3bd75955c08b366faebf99c0
owaiskhilji <owaiskhilji6@gmail.com>
2024-03-24 01:06:52 +0500

    2nd commit

--- index.js
+++ index.js
@@ -2,16 +2,30 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 let rondomnum = Math.floor(Math.random() * 4 + 1);
-let user = await inquirer.prompt([
-    {
-        name: "Enterthenumber",
-        type: "number",
-        message: "put the number  1 to 4 :"
+let users = async () => {
+    let user = await inquirer.prompt([
+        {
+            name: "Enterthenumber",
+            type: "number",
+            message: "put the number  1 to 4 :"
+        }
+    ]);
+    if (user.Enterthenumber === rondomnum) {
+        console.log(chalk.bgGreen(`congratulation you win Because anwer is ${rondomnum} match`));
     }
-]);
-if (user.Enterthenumber === rondomnum) {
-    console.log(chalk.bgGreen("congratulation you win"));
-}
-else {
-    console.log(chalk.bgRed(`Try again: correct anwer is ${rondomnum}`));
-}
+    else {
+        console.log(chalk.bgRed(`Try again: correct anwer is ${rondomnum} `));
+    }
+};
+let confirm;
+do {
+    await users();
+    confirm = await inquirer.prompt([
+        {
+            name: "con",
+            type: 'confirm',
+            message: "Do you want to start :"
+        }
+    ]);
+} while (confirm.con);
+console.log(confirm);
--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@
 import inquirer from "inquirer"
 import chalk from "chalk";
 let rondomnum = Math.floor(Math.random()*4+1)
+
+
+let users = async () =>{
 let user = await inquirer.prompt([
     {
         name : "Enterthenumber",
@@ -10,9 +13,28 @@ let user = await inquirer.prompt([
     }
 ])
 
+
 if(user.Enterthenumber === rondomnum){
-    console.log(chalk.bgGreen("congratulation you win"));
+    console.log(chalk.bgGreen(`congratulation you win Because anwer is ${rondomnum} match`));
 }
 else{
     console.log(chalk.bgRed(`Try again: correct anwer is ${rondomnum} `));
 }
+}
+
+
+
+let confirm ;
+do{
+   await users()
+    confirm = await inquirer.prompt([
+        {
+            name : "con",
+            type : 'confirm',
+            message : "Do you want to start :"
+        }
+    ])
+}
+
+while(confirm.con)
+console.log(confirm);
